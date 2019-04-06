7a6161d7c9433c182d8b0e206e0aa4b6279b12bd
javo <interactionmx@gmail.com>
2019-04-06 17:46:59 -0600

    added initial config for processing CSS and images

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+
+function component() {
+    let element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // add img to existing div 
+
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    return element;
+}
+
+
+
+document.body.appendChild(component());
